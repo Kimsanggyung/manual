1610daf6608cffae63f343d739e709a45646f5eb
Kim sanggyung <ksang0906@gmail.com>
2022-03-03 13:56:30 +0900

    함수하나로 수정

--- cpr.js
+++ cpr.js
@@ -1,31 +1,6 @@
 function backToMain() {
     document.location.href='main.html'
 }
-
-function cpr_page1(){
-    document.location.href='cprStep1.html'
-}
-
-function cpr_page2(){
-    document.location.href='cprStep2.html'
-}
-
-function cpr_page3(){
-    document.location.href='cprStep3.html'
-}
-
-function cpr_page4(){
-    document.location.href='cprStep4.html'
-}
-
-function cpr_page5(){
-    document.location.href='cprStep5.html'
-}
-
-function cpr_page6(){
-    document.location.href='cprStep6.html'
-}
-
-function cpr_page7(){
-    document.location.href='cprStep7.html'
+function next(step){
+    document.location.href= step
 }
